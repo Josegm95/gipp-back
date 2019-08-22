e2fb5aac5c74e686d4e1902f8f39b8c654befee8
Jose Gomez <jose.fgm1995@gmail.com>
2019-08-21 23:19:16 -0500

    add cors package

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = require('./routes/router');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(router);
 
+app.use(router);
+app.use(cors());
 app.listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`);
 });
