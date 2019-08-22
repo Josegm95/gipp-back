7e902b3e251f752ddcc43d179c5daaa20a2e0605
Jose Gomez <jose.fgm1995@gmail.com>
2019-08-22 10:58:35 -0500

    query problem solve and cors test

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = require('./routes/router');
-const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(router);
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(router);
 
 app.listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`);
