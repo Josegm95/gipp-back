1b4bb2afb6e09d1eb116dc6a976fb046a0b70101
Jose Gomez <jose.fgm1995@gmail.com>
2019-08-21 23:47:31 -0500

    add cors manualy

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(router);
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`);
 });
